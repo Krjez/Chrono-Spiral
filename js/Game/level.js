c0d5279ec330ba33f097aa84669bd4d9cfae97a7
Jakub Polacek <Kuba.krtek@email.cz>
2023-12-13 01:29:58 +0100

    Animator scrapped for now, changing focus on player movement

--- js/Game/level.js
+++ js/Game/level.js
@@ -30,20 +30,20 @@ class Level extends Game {
 
     // Create platforms and add them to the game
     const platforms = [
-      new Platform(0, this.canvas.height - 20, platformWidth, 20),
-      new Platform(platformWidth + gap, this.canvas.height - 20, platformWidth, 20),
-      new Platform(2 * (platformWidth + gap), this.canvas.height - 20, platformWidth, 20),
-      new Platform(3 * (platformWidth + gap), this.canvas.height - 20, platformWidth, 20),
-      new Platform(4 * (platformWidth + gap), this.canvas.height - 20, platformWidth, 20),
+      new Platform(0, this.canvas.height - 20, 1500, 20),
+      //new Platform(platformWidth + gap, this.canvas.height - 20, platformWidth, 20),
+      //new Platform(2 * (platformWidth + gap), this.canvas.height - 20, platformWidth, 20),
+      //new Platform(3 * (platformWidth + gap), this.canvas.height - 20, platformWidth, 20),
+      //new Platform(4 * (platformWidth + gap), this.canvas.height - 20, platformWidth, 20),
     ];
     for (const platform of platforms) {
       this.addGameObject(platform);
     }
 
     // Create enemies and add them to the game
-    this.addGameObject(new Enemy(50, this.canvas.height - 90));
-    this.addGameObject(new Enemy(platformWidth + gap + 50, this.canvas.height - 90));
-    this.addGameObject(new Enemy(2 * (platformWidth + gap) + 50, this.canvas.height - 90));
+    //this.addGameObject(new Enemy(50, this.canvas.height - 90));
+    //this.addGameObject(new Enemy(platformWidth + gap + 50, this.canvas.height - 90));
+    //this.addGameObject(new Enemy(2 * (platformWidth + gap) + 50, this.canvas.height - 90));
 
     // Create collectibles and add them to the game
     this.addGameObject(new Collectible(250, this.canvas.height - 100, 20, 20));
